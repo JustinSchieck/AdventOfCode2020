80617be6ccd0f73a0e7dd6f7ddd36e9a3615037c
Schieck, Justin <justin_schieck@manulife.ca>
2020-12-08 14:54:38 -0500

    finished day 1

--- Day1/day1.js
+++ Day1/day1.js
@@ -1,17 +1,3 @@
-/**
-Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers together.
-For example, suppose your expense report contained the following:
-
-1721
-979
-366
-299
-675
-1456
-
-In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying them together produces 1721 * 299 = 514579, so the correct answer is 514579.
-Of course, your expense report is much larger. Find the two entries that sum to 2020; what do you get if you multiply them together?
- */
 
 export const expenseReport = (data) => {
     const dataArray = data.split('\n').map(number => parseInt(number, 10))
@@ -20,6 +6,20 @@ export const expenseReport = (data) => {
         let target = 2020 - dataArray[i];
         if (dataArray.includes(target)) {
             return dataArray[i] * target;
+        }
+    }
+};
+
+
+export const expenseReportThree = (data) => {
+    const dataArray = data.split('\n').map(number => parseInt(number, 10))
+
+    for (let i = 0; i <= dataArray.length; i++) {
+        for (let n = 0; n <= dataArray.length; n++) {
+            let target = 2020 - dataArray[i] - dataArray[n];
+            if (dataArray.includes(target)) {
+                return dataArray[i] * dataArray[n] * target;
+            }
         }
     }
 };
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import { expenseReport } from './Day1/day1.js';
+import { expenseReport, expenseReportThree } from './Day1/day1.js';
 import { report } from './data.js';
 
-console.log(expenseReport(report));
+// console.log(expenseReport(report));
+console.log(expenseReportThree(report));
