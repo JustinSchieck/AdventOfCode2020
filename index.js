2c0cfa67fa6734abde53aa4e325a0d1c82d5f319
Schieck, Justin <justin_schieck@manulife.ca>
2020-12-09 13:11:09 -0500

    finished day 1

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-import { expenseReport, expenseReportThree } from './Day1/day1.js';
+import { expenseReport, expenseReportThree } from './Day1/index.js';
 import { report } from './data.js';
 
+/**
+ * Day 1 - Expense Report
+ */
+
 // console.log(expenseReport(report));
-console.log(expenseReportThree(report));
+// console.log(expenseReportThree(report));
+
+/**
+ * Day 2 - 
+ */
+
+console.log();
+// console.log();
