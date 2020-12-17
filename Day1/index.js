9764cf4d3fc966dcda6a068558d07ad043684b59
Schieck, Justin <justin_schieck@manulife.ca>
2020-12-17 12:07:40 -0500

    updating packages for better use.

--- Day1/index.js
+++ Day1/index.js
@@ -1,4 +1,4 @@
-
+import { report } from './input.js';
 export const expenseReport = (data) => {
     const dataArray = data.split('\n').map(number => parseInt(number, 10))
 
@@ -14,12 +14,17 @@ export const expenseReport = (data) => {
 export const expenseReportThree = (data) => {
     const dataArray = data.split('\n').map(number => parseInt(number, 10))
 
+    const awnserOne = expenseReport(data);
+    let awnserTwo = 0;
     for (let i = 0; i <= dataArray.length; i++) {
         for (let n = 0; n <= dataArray.length; n++) {
             let target = 2020 - dataArray[i] - dataArray[n];
             if (dataArray.includes(target)) {
-                return dataArray[i] * dataArray[n] * target;
+                awnserTwo = dataArray[i] * dataArray[n] * target;
             }
         }
     }
+    return `Expense Report One: ${awnserOne}, Expense Report Two: ${awnserTwo}`;
 };
+
+console.log(expenseReportThree(report));
---
+++ Day2/index.js
@@ -0,0 +1,7 @@
+import { inputData } from './input';
+
+const passwordValidator = passList => {
+    return passList;
+};
+
+console.log(passwordValidator(inputData));
